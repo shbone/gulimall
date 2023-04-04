51a8437313a8cbd0daf8d365ca9892a0005fce7a
shbone <598924626@qq.com>
2023-04-04 23:36:53 +0800

    第一次提交

---
+++ src/main/java/com/example/gulimall3/Gulimall3Application.java
@@ -0,0 +1,13 @@
+package com.example.gulimall3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gulimall3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gulimall3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gulimall3/Gulimall3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimall3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gulimall3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
