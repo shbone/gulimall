9dc9f9f2c29bd00f894964f310c0f169853028c2
shbone <598924626@qq.com>
2023-04-07 21:20:05 +0800

    gateway configure

---
+++ gulimall-gateway/src/main/java/com/example/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.example.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1. 配置注册发现发现（nacos)
+ */
+
+//@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
