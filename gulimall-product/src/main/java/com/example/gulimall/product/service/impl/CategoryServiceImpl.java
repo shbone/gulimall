0c9d3e234e0ecd0733f013e436c8c5ed4be71f0a
shbone <598924626@qq.com>
2023-04-17 01:03:38 +0800

    three menus

--- gulimall-product/src/main/java/com/example/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.example.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +28,36 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listwithTree() {
+//        1. 查出所有分类
+        List<CategoryEntity> entityList =baseMapper.selectList(null);
+//        2. 构建成树形结构
+//        2.1 选出一级分类
+        List<CategoryEntity> level1Menus = entityList.stream().filter((CategoryEntity)->
+            CategoryEntity.getParentCid() == 0
+        ).map((menu) ->{
+            menu.setChildren(getChildrens(menu,entityList));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+        return level1Menus;
+    }
+
+    List<CategoryEntity>  getChildrens(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> children=all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid().equals(root.getCatId()) ;
+        }).map(categoryEntity -> {
+            categoryEntity.setChildren(getChildrens(categoryEntity,all));
+            return categoryEntity;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
