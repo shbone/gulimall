d0cd3cd64bffedfb3bb335d45eeec987fbe34cdf
shbone <598924626@qq.com>
2023-04-19 00:30:52 +0800

    tree show

--- gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
@@ -3,7 +3,9 @@ package com.example.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.example.gulimall.product.dao")
 public class GulimallProductApplication {
